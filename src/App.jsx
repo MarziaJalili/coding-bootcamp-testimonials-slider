51e9563b6dab9083e1d7b62e2b984f50fb907172
Marzia Jalili <astronomyfancy@gmail.com>
2025-04-05 15:56:16 +0430

    finished the mobile design and the logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import data from "./data.js";
 
 function App() {
   const [count, setCount] = useState(0);
+  const item = data[count];
+
+  const toggleUsers = (button) => {
+    button === "next" ? setCount(1) : setCount(0);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="font-Inter text-Dark-Blue bg-[url(../public/pattern-curve.svg)] h-[100vh] bg-no-repeat bg-position-[bottom_0_left_0] bg-size-[80%] grid place-items-center">
+      <article className="flex flex-col space-y-12 container mx-auto px-6">
+        <section className="relative bg-[url(../public/pattern-bg.svg)] p-10 bg-no-repeat bg-position-[center] bg-size-[100%]">
+          {/* user image */}
+
+          <img
+            className="rounded-lg shadow-[0_30px_80px_-40px_hsl(245,50%,49%)]"
+            src={item.img.src}
+            alt={item.img.alt}
+          />
+
+          {/* toggle buttons... */}
+          <div className="flex justify-between space-x-12 absolute py-4 px-6 bg-white bottom-4 translate-x-[-50%] left-[50%] rounded-full">
+            <button
+              onClick={() => toggleUsers("prev")}
+              aria-label="switches the slides"
+              className="cursor-pointer hover:scale-120  transition-all duration-300 "
+            >
+              <img className="" src="./icon-prev.svg" alt="icon-prev" />
+            </button>
+            <button
+              onClick={() => toggleUsers("next")}
+              aria-label="switches the slides"
+              className="cursor-pointer hover:scale-120 transition-all duration-300 "
+            >
+              <img className="" src="./icon-next.svg" alt="icon-next" />
+            </button>
+          </div>
+        </section>
+
+        <div className="flex flex-col py-6 items-center text-center space-y-6 container mx-auto bg-[url(../public/pattern-quotes.svg)] bg-no-repeat bg-position-[center_top] bg-size-[20%]">
+          <p className="text-xl font-light">{item.paragraph}</p>
+
+          <strong className="text-lg">
+            {item.name}
+            <span className="block text-Grayish-Blue font-light">
+              {item.position}
+            </span>
+          </strong>
+        </div>
+      </article>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,25 @@
+
+const data = [
+  {
+    img: {
+      src: "./image-tanya.jpg",
+      alt: "image-tanya",
+    },
+    paragraph:
+      "“ I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future. ”",
+    name: "Tanya Sinclair",
+    position: "UX Engineer",
+  },
+  {
+    img: {
+      src: "./image-john.jpg",
+      alt: "image-john",
+    },
+    paragraph:
+      "“ If you want ot lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+    name: "John Tarkpor",
+    position: "Junior Front-end Developer",
+  },
+];
+
+export default data
